e6c6e83c6a46e2d764347c512287d3c9258013be
Mitsuru Kusumoto <a@a>
2019-05-04 20:52:23 +0900

    stop using ffmpeg_extract_subclip

--- scripts/cut.py
+++ scripts/cut.py
@@ -1,6 +1,7 @@
 import sys
 
-from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+# from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+from moviepy.editor import VideoFileClip
 
 
 in_video, out_video, t1, t2 = sys.argv[1:5]
@@ -8,4 +9,6 @@ t1 = float(t1)
 t2 = float(t2)
 assert in_video != out_video
 
-ffmpeg_extract_subclip(in_video, t1, t2, targetname=out_video)
+video = VideoFileClip(in_video)
+video = video.subclip(t1, t2)
+video.write_videofile(out_video, audio=False)
